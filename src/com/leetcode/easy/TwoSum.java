7ec50b0e90c9913c3bf18cc8182736d75102ec4a
Developer-Y <developer.yp@gmail.com>
2019-06-28 00:17:54 +0530

    01 Two Sum

---
+++ src/com/leetcode/easy/TwoSum.java
@@ -0,0 +1,20 @@
+package com.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int index = 0; index < nums.length; index++){
+            int current = nums[index];
+            if(map.containsKey(target - current)){
+                return new int[]{map.get(target-current), index};
+            }else{
+                map.put(current, index);
+            } 
+        }
+        return null;
+    }
+}
